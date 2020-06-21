8a7b7554a91a679fde5f131bf138a46aac7ce119
Dmitriy Morozov <dmitriy@mrzv.org>
2020-06-20 19:32:09 -0700

    Add a very basic repl

--- saturn.py
+++ saturn.py
@@ -5,6 +5,14 @@ import  argh
 from    rich.console import Console
 from    rich.rule    import Rule
 
+import  io
+
+from    prompt_toolkit import PromptSession
+from    pygments.lexers.python import PythonLexer
+from    prompt_toolkit.lexers import PygmentsLexer
+from    prompt_toolkit.key_binding import KeyBindings
+from    prompt_toolkit.shortcuts import message_dialog
+
 from    icecream import ic
 
 from    lib import  cells as c, notebook
@@ -52,12 +60,15 @@ def show(fn, html = '', debug = False):
 
 @argh.arg('outfn', nargs='?')
 @argh.arg('-n', '--dry-run')
-def run(infn, outfn, clean: "run from scratch, ignoring checkpoints" = False, debug = False, dry_run = False):
+def run(infn, outfn, clean: "run from scratch, ignoring checkpoints" = False, debug = False, dry_run = False, repl = False):
     if not outfn:
         outfn = infn
 
-    with open(infn) as f:
-        cells = c.parse(f)
+    if os.path.exists(infn):
+        with open(infn) as f:
+            cells = c.parse(f)
+    else:
+        cells = []
 
     output = lambda cell: show_console(cell, rule = debug, verbose = debug)
 
@@ -73,8 +84,47 @@ def run(infn, outfn, clean: "run from scratch, ignoring checkpoints" = False, de
 
     nb.process(output)
 
+    if repl:
+        run_repl(nb, output, outfn, dry_run)
+
     if not dry_run:
         nb.save(outfn)
+
+def run_repl(nb, output, outfn = '', dry_run = True):
+    session = PromptSession()
+
+    # Swap enter and meta+enter #
+    kb = KeyBindings()
+
+    @kb.add('escape', 'enter')
+    def _(event):
+        event.current_buffer.insert_text('\n')
+
+    @kb.add('enter')
+    def _(event):
+        event.current_buffer.validate_and_handle()
+
+    @kb.add('c-s')
+    def _(event):
+        if not dry_run:
+            nb.save(outfn)
+    #############################
+
+    while True:
+        try:
+            code = session.prompt('>>> ',
+                                  lexer = PygmentsLexer(PythonLexer),
+                                  key_bindings = kb,
+                                  multiline = True,
+                                  bottom_toolbar = None if not dry_run else 'Dry-run mode')
+        except KeyboardInterrupt:
+            continue
+        except EOFError:
+            break
+        else:
+            cells = c.parse(io.StringIO(code))
+            nb.add(cells)
+            nb.process(output)
 
 if __name__ == '__main__':
     argh.dispatch_commands([show, run])
