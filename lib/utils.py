ab5b2d6f980f2c5b06f637e47327da06a6e2b40e
Dmitriy Morozov <dmitriy@mrzv.org>
2020-06-23 16:09:34 -0700

    Get rid of the unused utils.pairwise()

--- lib/utils.py
+++ lib/utils.py
@@ -13,13 +13,6 @@ def stdIO(stdout=None, stderr=None):
 
     sys.stdout = old
 
-from itertools import tee, chain
-def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = tee(chain(iterable, [None]))
-    next(b, None)
-    return zip(a, b)
-
 # From: https://stackoverflow.com/a/18854817/44738
 def chunkstring(string, length):
     return (string[0+i:length+i] for i in range(0, len(string), length))
