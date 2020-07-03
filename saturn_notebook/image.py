cc35aae92cceff54ce4ca1f644d5f4e1b1c7f337
Dmitriy Morozov <dmitriy@mrzv.org>
2020-07-03 15:34:10 -0700

    Implement icat in pure Python instead of relying on an external command

--- saturn_notebook/image.py
+++ saturn_notebook/image.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 import io
 import os
 import sys
-from subprocess import run
+from base64 import standard_b64encode
+from . import utils
+from more_itertools import peekable
 
 enabled = os.environ['TERM'] == 'xterm-kitty' and sys.stdout.isatty()      # TODO: find a better way
 seen = []
@@ -27,4 +29,25 @@ def save_mpl_png():
 
 def show_png(buf):
     if enabled:
-        run(['kitty', '+kitten', 'icat'], input = buf)
+        icat(buf)
+
+def icat(buf):
+    def serialize_gr_command(cmd, payload=None):
+       cmd = ','.join('{}={}'.format(k, v) for k, v in cmd.items())
+       ans = io.BytesIO()
+       w = ans.write
+       w(b'\033_G'), w(cmd.encode('ascii'))
+       if payload:
+          w(b';')
+          w(payload)
+       w(b'\033\\')
+       return ans.getbuffer()
+
+    pchunk = peekable(utils.chunkstring(standard_b64encode(buf), 4096))
+    cmd = { 'a': 'T', 'f': 100 }
+    for chunk in pchunk:
+        cmd['m'] = 1 if pchunk else 0
+        sys.stdout.buffer.write(serialize_gr_command(cmd, chunk))
+        cmd.clear()
+    sys.stdout.write('\n')
+    sys.stdout.flush()
