c7c1f36368610bc01996940fca425e6df0ed7de2
Dmitriy Morozov <dmitriy@mrzv.org>
2020-07-17 20:01:14 -0700

    Remember webview window shape

--- saturn_notebook/viewer.py
+++ saturn_notebook/viewer.py
@@ -6,5 +6,30 @@ from    atomicwrites import atomic_write
 config_path = AppDirs('saturn', 'mrzv').user_config_dir
 
 def view(html):
-    window = webview.create_window('Saturn', html = html)
+    x = None
+    y = None
+    width = 800
+    height = 600
+
+    try:
+        if not os.path.exists(config_path): os.makedirs(config_path)
+        shape_fn = os.path.join(config_path, 'window-shape')
+
+        if os.path.exists(shape_fn):
+            with open(shape_fn, 'r') as f:
+                [x, y, width, height] = map(int, f.readlines())
+    except:
+        pass
+
+    window = webview.create_window('Saturn', html = html, x = x, y = y, width = width, height = height)
+
+    def save_shape():
+        with atomic_write(shape_fn, mode='w', overwrite=True) as of:
+            of.write(f'{int(window.x)}\n')
+            of.write(f'{int(window.y)}\n')
+            of.write(f'{int(window.width)}\n')
+            of.write(f'{int(window.height)}\n')
+
+    window.closing += save_shape
+
     webview.start()
