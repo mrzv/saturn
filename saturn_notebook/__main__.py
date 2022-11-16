0147b2369b62520438b0a7875cfcda922f8a6a40
Dmitriy Morozov <dmitriy@mrzv.org>
2022-11-15 21:23:28 -0800

    Add option to embed KaTeX into HTML output

--- saturn_notebook/__main__.py
+++ saturn_notebook/__main__.py
@@ -56,8 +56,32 @@ def show_console(cell, rule = False, verbose = False, no_show = False):
 def show_html(cell, f):
     cell.show_html(f)
 
+katex_preamble = r"""
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.3/dist/katex.min.css" integrity="sha384-Juol1FqnotbkyZUT5Z7gUPjQ9gzlwCENvUZTpQBAPxtusdwFLRy382PSDx5UUJ4/" crossorigin="anonymous">
+<script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.3/dist/katex.min.js" integrity="sha384-97gW6UIJxnlKemYavrqDHSX3SiygeOwIZhwyOKRfSaf0JWKRVj9hLASHgFTzT+0O" crossorigin="anonymous"></script>
+<script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.3/dist/contrib/auto-render.min.js" integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05" crossorigin="anonymous"></script>
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        renderMathInElement(document.body, {
+          // customised options
+          // • auto-render specific keys, e.g.:
+          delimiters: [
+              {left: '$$', right: '$$', display: true},
+              {left: '$', right: '$', display: false},
+              {left: '\\(', right: '\\)', display: false},
+              {left: '\\[', right: '\\]', display: true}
+          ],
+          // • rendering keys, e.g.:
+          throwOnError : false
+        });
+    });
+</script>
+<script src="https://cdn.jsdelivr.net/npm/katex@0.16.3/dist/contrib/copy-tex.min.js" integrity="sha384-ww/583aHhxWkz5DEVn6OKtNiIaLi2iBRNZXfJRiY1Ai7tnJ9UXpEsyvOITVpTl4A" crossorigin="anonymous"></script>
+"""
+
 def show(fn: "input notebook",
          html: "save HTML to a file" = '',
+         katex: "include KaTeX in HTML output" = False,
          debug: "show debugging information" = False):
     """Show the contents of the notebook, without evaluating."""
     with open(fn) as f:
@@ -69,8 +93,11 @@ def show(fn: "input notebook",
         f_html = open(html, 'w')
         output = lambda cell: show_html(cell, f_html)
 
+        f_html.write('<!DOCTYPE html>\n')
         f_html.write('<html>\n')
         f_html.write('<head>\n')
+        if katex:
+            f_html.write(katex_preamble)
         f_html.write('<style>\n')
         f_html.write(c.HtmlFormatter().get_style_defs('.highlight'))
         f_html.write('</style>\n')
